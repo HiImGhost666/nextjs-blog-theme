a4988bcf30f81c9609f949e07a96b477726be2fd
Yoshi <93407449+HiImGhost666@users.noreply.github.com>
2022-04-15 12:41:11 +0100

    Update ArrowIcon.js

--- components/ArrowIcon.js
+++ components/ArrowIcon.js
@@ -3,7 +3,7 @@ export default function ArrowIcon({ className, color = 'text-primary' }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
-      height="24"
+      height="44"
       fill="none"
       viewBox="0 0 24 24"
       className={className}
