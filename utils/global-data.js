3ae8b9cffab1243639a57dc6c8569eacb498a51b
Yoshi <93407449+HiImGhost666@users.noreply.github.com>
2022-04-16 22:52:08 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -8,7 +8,7 @@ export const getGlobalData = () => {
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
-
+<link rel="icon" type="image/jpg" href="https://github.com/HiImGhost666/nextjs-blog-theme/blob/9802850eb5df694abaaf17c206468cd17d9af019/assets/why.jpg"/>
   return {
     name,
     blogTitle,
